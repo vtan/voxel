73dad1368fc184c7d6a7b8cedfa0fcf6fb510efe
Viktor Tanyi <tanyi.viktor@gmail.com>
2016-10-23 17:08:30 +0200

    Refactor mesh building

--- src/mesh_builder.hpp
+++ src/mesh_builder.hpp
@@ -12,26 +12,36 @@
 
 #include <glm/glm.hpp>
 
+struct Vec3Comparator
+{
+    bool operator()(const glm::vec3& v1, const glm::vec3& v2) const
+    {
+        return v1.x != v2.x ? v1.x < v2.x
+            : v1.y != v2.y ? v1.y < v2.y
+            : v1.z < v2.z;
+    }
+};
+
 class MeshBuilder
 {
 public:
     MeshData build(const Volume<Voxel>);
 private:
+    MeshData mesh_data;
+    std::map<glm::vec3, GLubyte, Vec3Comparator> vertices_with_brightness;
+
+    void non_empty_voxel(const Volume<Voxel>&, glm::ivec3, glm::vec3);
+    void vertex(const Volume<Voxel>&, glm::ivec3);
+
     static const std::vector<std::pair<glm::ivec3, std::vector<glm::vec3>>>
         neighbor_dirs_with_face_vertex_positions;
-
-    void add_positions_for_surrounding_face(
-            const Volume<Voxel>&,
-            glm::ivec3,
-            glm::vec3,
-            std::back_insert_iterator<std::vector<glm::vec3>>);
 };
 
 // The border voxels are considered neighbors and are not included in the mesh.
 MeshData MeshBuilder::build(const Volume<Voxel> volume)
 {
-    MeshData data;
-    auto pos_inserter = std::back_inserter(data.positions);
+    mesh_data = {};
+    vertices_with_brightness = {};
 
     volume.for_each_voxel_in_border(1, 1, 1, [&](auto x, auto y, auto z) {
         const glm::ivec3 current_idx(x, y, z);
@@ -39,63 +49,32 @@ MeshData MeshBuilder::build(const Volume<Voxel> volume)
         const Voxel current = volume.at(current_idx);
 
         if (current != Voxel::empty) {
-            this->add_positions_for_surrounding_face(
-                    volume, current_idx, current_pos, pos_inserter);
+            this->non_empty_voxel(volume, current_idx, current_pos);
         }
     });
-
-    auto vec3_cmp = [](auto v1, auto v2) {
-        return v1.x != v2.x ? v1.x < v2.x
-            : v1.y != v2.y ? v1.y < v2.y
-            : v1.z < v2.z;
-    };
-    std::map<glm::vec3, GLubyte, decltype(vec3_cmp)>
-        vertices_with_brightness(vec3_cmp);
 
     volume.for_each_vertex_in_border(1, 1, 1, [&](auto x, auto y, auto z) {
-        const glm::vec3 vertex(x - 1, y - 1, z - 1);
-        GLubyte nonempty_neighbor_voxel_count = 0;
-
-        for (int dz = -1; dz <= 0; ++dz) {
-            for (int dy = -1; dy <= 0; ++dy) {
-                for (int dx = -1; dx <= 0; ++dx) {
-                    const glm::ivec3 neighbor_voxel_pos(x + dx, y + dy, z + dz);
-                    const Voxel neighbor = volume.at(neighbor_voxel_pos);
-
-                    if (neighbor != Voxel::empty) {
-                        ++nonempty_neighbor_voxel_count;
-                    }
-                }
-            }
-        }
-
-        if (nonempty_neighbor_voxel_count > 0
-                && nonempty_neighbor_voxel_count < 8) {
-            // Mapping values from 0 (dark) to 255 (bright).
-            GLubyte brightness = (8 - nonempty_neighbor_voxel_count) << 5;
-            vertices_with_brightness[vertex] = brightness;
-        }
+        const glm::ivec3 current_idx(x, y, z);
+        this->vertex(volume, current_idx);
     });
 
-    auto brightness_inserter = std::back_inserter(data.brightnesses);
     std::transform(
-            data.positions.begin(),
-            data.positions.end(),
-            brightness_inserter,
+            mesh_data.positions.begin(),
+            mesh_data.positions.end(),
+            std::back_inserter(mesh_data.brightnesses),
             [&](auto v) {
                 assert(vertices_with_brightness.count(v) == 1);
 
                 return vertices_with_brightness[v];
             });
 
-    return data;
+    return mesh_data;
 }
 
-void MeshBuilder::add_positions_for_surrounding_face(
+void MeshBuilder::non_empty_voxel(
         const Volume<Voxel> &volume,
         const glm::ivec3 current_idx,
-        const glm::vec3 current_pos,
-        std::back_insert_iterator<std::vector<glm::vec3>> pos_inserter)
+        const glm::vec3 current_pos)
 {
     for (auto neighbor : neighbor_dirs_with_face_vertex_positions) {
         const auto neighbor_idx = current_idx + neighbor.first;
@@ -104,9 +83,40 @@ void MeshBuilder::add_positions_for_surrounding_face(
             std::transform(
                     rel_positions.begin(),
                     rel_positions.end(),
-                    pos_inserter,
+                    std::back_inserter(mesh_data.positions),
                     [=](auto rel_pos) { return current_pos + rel_pos; });
         }
+    }
+}
+
+void MeshBuilder::vertex(
+        const Volume<Voxel> &volume, const glm::ivec3 current_idx)
+{
+    const glm::vec3 vertex(
+            current_idx.x - 1, current_idx.y - 1, current_idx.z - 1);
+    GLubyte nonempty_neighbor_voxel_count = 0;
+
+    for (int dz = -1; dz <= 0; ++dz) {
+        for (int dy = -1; dy <= 0; ++dy) {
+            for (int dx = -1; dx <= 0; ++dx) {
+                const glm::ivec3 neighbor_voxel_pos(
+                        current_idx.x + dx,
+                        current_idx.y + dy,
+                        current_idx.z + dz);
+                const Voxel neighbor = volume.at(neighbor_voxel_pos);
+
+                if (neighbor != Voxel::empty) {
+                    ++nonempty_neighbor_voxel_count;
+                }
+            }
+        }
+    }
+
+    if (nonempty_neighbor_voxel_count > 0
+            && nonempty_neighbor_voxel_count < 8) {
+        // Mapping values from 0 (dark) to 255 (bright).
+        GLubyte brightness = (8 - nonempty_neighbor_voxel_count) << 5;
+        vertices_with_brightness[vertex] = brightness;
     }
 }
 
